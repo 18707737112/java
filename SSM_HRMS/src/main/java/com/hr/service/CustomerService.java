2e4deeaf2110a4a93ec430d9a52748ce0e3c6613
Your Name <you@example.com>
2019-05-16 11:27:25 +0800

    登陆

--- SSM_HRMS/src/main/java/com/hr/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.hr.service;
-
-import com.hr.dao.CustomerDao;
-import com.hr.model.HRM_customer;
-
-public interface CustomerService {
-    CustomerDao ligon(HRM_customer customer);
-
-}
